dd023c9a690084f482ee863281bdbde7dfd1ea6f
Siro Pregno <siropregno14@gmail.com>
2025-06-22 05:46:15 -0300

    main src

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/clanceibo/', // Asegúrate de que coincida con el nombre del repositorio
+  base: '/clanceibo/', // Cambia esto al nombre de tu repositorio
   plugins: [react()],
 });
